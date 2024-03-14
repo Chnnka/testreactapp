b136273136effd8b69f43ec48f4a0e56d1e140e6
Channaka Gunawardhane <channakagunawardana@gmail.com>
2024-03-14 11:28:14 +0530

    User table and form

---
+++ src/UserForm.js
@@ -0,0 +1,60 @@
+import { Button, Grid, Input, Typography } from "@mui/material";
+import { useState } from "react";
+
+const UserForm = props =>{
+  const [id,setId]=useState();
+  return(
+    <Grid container spacing={2} sx={{backgroundColor:"#fff",marginBottom:'30px',display:'block',}}>
+      <Grid item xs={12}>
+        <Typography 
+          component={'h1'} 
+          sx={{color:'#000'}}
+        >
+            User Form
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={6} sx={{display:'flex'}}>
+        <Typography 
+        component={'label'} 
+        htmlFor="id" 
+        sx={{
+              color:'#000',
+              marginRight:'20px',
+              fontSize:'16px',
+              width:'100px',
+              display:'block'}}
+        >
+          ID
+        </Typography>
+        <Input 
+          type="number" 
+          id="id" 
+          name="id"
+          sx={{width:'400px'}} 
+          value={''} 
+          onChange={e=>{}}/>
+      </Grid>
+      <Grid item xs={12} sm={6} sx={{display:'flex'}}>
+        <Typography component={'label'} htmlFor="name" sx={{color:'#000',marginRight:'20px',fontSize:'16px',width:'100px',display:'block'}}>
+          Name
+        </Typography>
+        <Input type="text" id="name" name="name"sx={{width:'400px'}} value={''} onChange={e=>{}}/>
+      </Grid>
+      <Button 
+        sx={{
+          margin:'auto', 
+          marginBottom:'20px', 
+          backgroundColor:'#00c6c6', 
+          color:'#000', 
+          marginLeft:'15px', 
+          '&:hover':{
+            opacity:'0.7',
+            backgroundColor:'#00c6c6'}}} 
+            onClick={''}>
+              Add
+      </Button>
+    </Grid>
+  );
+}
+
+export default UserForm;
